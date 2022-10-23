45791a3cb9ff2383ad4c765b0d0a94b763ee62f7
mikhailovaNV <fefu.mikhailova@feip.school>
2022-10-23 22:45:50 +1000

    no message

--- farpostTestProj/Utils/DataManager.swift
+++ farpostTestProj/Utils/DataManager.swift
@@ -2,5 +2,10 @@ import UIKit
 
 struct DataManager {
     
-    let images: [String] = ["https://upload.wikimedia.org/wikipedia/commons/1/13/Rhamnus_frangula_13_ies.jpg", "https://upload.wikimedia.org/wikipedia/commons/2/2e/Steel-bridge-800x600.JPG", "https://upload.wikimedia.org/wikipedia/commons/5/5a/Euphorbia_squarrosa.jpeg", "https://petapixel.com/assets/uploads/2020/12/riverjpg-800x600.jpg", "https://aviationweek.com/sites/default/files/styles/crop_freeform/public/2022-10/atlas_air_747-8f_source_boeing.jpeg?itok=vzYTtPpU"]
-}
+    let images: [String] = [
+        "https://upload.wikimedia.org/wikipedia/commons/1/13/Rhamnus_frangula_13_ies.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/2/2e/Steel-bridge-800x600.JPG",
+        "https://upload.wikimedia.org/wikipedia/commons/5/5a/Euphorbia_squarrosa.jpeg",
+        "https://petapixel.com/assets/uploads/2020/12/riverjpg-800x600.jpg",
+        "https://aviationweek.com/sites/default/files/styles/crop_freeform/public/2022-10/atlas_air_747-8f_source_boeing.jpeg?itok=vzYTtPpU"
+    ]}
